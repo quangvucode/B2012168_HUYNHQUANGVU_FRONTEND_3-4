5bf2401236e5c33cf38fa798e4a93a6c3e4cebde
quangvucode <quangvu36zz@gmail.com>
2024-10-30 18:31:43 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+    <p>Ôi giời đất ơi, không thể tìm thấy trang. <br>
+        Trở về
+    <router-link to="/">Trang chủ.</router-link>
+    </p>
+    </div>
+</template>
